c1dda39fec7ceaf5a591feab71e61bb0e5de6f8f
Bob Oyier <boboyier92@gmail.com>
2024-07-28 17:58:00 +0300

    Starter Code

---
+++ README.md
@@ -0,0 +1,103 @@
+# Todo App
+
+**NOTE: THIS IS THE STARTER CODE. FOR EACH CONCEPT COVERED, VIEW THE RELEVANT BRANCHES.**
+
+## Overview
+
+In this mini-project, we'll cover the basics of React JS by rewriting the below Todo App written in HTML and Vanilla JS in React.
+
+![Preview of the Todo App as viewed on a Samsung Galaxy Fold 2 phone.](./src/assets/readme-images/todo-demo.png)
+
+## Topics
+
+1. Component Architecture
+2. JSX
+3. Prop Basics
+4. Destructuring Assignment
+5. Lists and Keys
+6. React Fragments
+7. React Children
+8. State
+9. Events in React
+10. Controlled Inputs
+11. `useEffect`
+12. React CRUD operations with JSON-server
+
+## System Requirements
+
+- Node 18+
+- JSON Server
+- Live Server extension (VS Code).
+- A browser capable of running JavaScript (Chrome, Firefox, Safari, or Edge)
+- Operating System (Windows 10+, MacOS, Linux, etc.)
+- A text editor capable of running JavaScript (Visual Studio Code, Vim, Nano, Emacs, Atom, Sublime Text, etc.)
+- RAM >= 4GB
+- Disk space >= 1GB
+
+## Installation
+
+To use this repo, follow these steps:
+
+### Alternative One
+
+1.  Open the terminal/CLI on your computer.
+
+2.  Clone the repository by running the following command:
+
+        git clone https://github.com/oyieroyier/todo-app.git
+
+3.  Change directory to the repo folder:
+
+        cd todo-app
+
+4.  Open it in your Code Editor of choice. If you use VS Code, run the command:
+
+        code .
+
+### Alternative Two
+
+- On the top right corner of this page there is a button labelled `Fork`.
+
+- Click on that button to create a copy of the repository to your own account.
+
+- Follow the process described in `Alternative One` above.
+
+- Remember to replace your username when cloning.
+
+      git clone https://github.com/your-github-username-here/todo-app.git
+
+## Running the application locally
+
+### Previewing the HTML/Vanilla JS version of the application.
+
+- In the file tree, click on the `html-demo` folder
+
+- Right click on the `todo.html` file to show the context menu and select `Open with Live Server` option.
+
+- This will open the file in your browser.
+
+- If using VS Code and don't have Live Server installed [CLICK HERE](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) to install it and repeat the second step.
+
+### Running the React Version
+
+- Open the integrated terminal in your code editor and install the project dependencies:
+
+      npm install
+
+- Once done with the installation, run the development server in your terminal.
+
+      npm run dev
+
+- You should see the notification `Your application running on port 5173 is available.` at the bottom of your editor.
+- If using VS Code, click the `Open in Browser` button.
+- If using any other editor, copy the link in the terminal and open it on your browser.
+
+      http://localhost:5173/
+
+When both versions of the application are running, you should see something similar on the browser windows where they are open.
+
+![Preview of the complete HTML/Vanilla JS version of the app and the React Vite starter Code](./src/assets/readme-images/preview.png)
+
+## Authors
+
+[Bob Oyier](https://github.com/oyieroyier/)
---
+++ html-demo/script.js
@@ -0,0 +1,138 @@
+/* eslint-disable no-unused-vars */
+const todos = [
+	{
+		id: 1,
+		title: 'Learn JavaScript Functions',
+		completed: false,
+	},
+	{
+		id: 2,
+		title: 'Learn JavaScript Array Methods',
+		completed: true,
+	},
+	{
+		id: 3,
+		title: 'Learn JavaScript Object Methods',
+		completed: false,
+	},
+	{
+		id: 4,
+		title: 'Learn JavaScript DOM Manipulation',
+		completed: true,
+	},
+	{
+		id: 5,
+		title: 'Learn JavaScript Events',
+		completed: false,
+	},
+];
+
+document.addEventListener('DOMContentLoaded', () => fetchAllTodos(todos));
+
+const todosContainer = document.querySelector('.todos-container');
+
+function fetchAllTodos(todosList) {
+	todosContainer.innerHTML = '';
+	todosList.map(createSingleTodoItem);
+}
+
+function createSingleTodoItem(todo, index) {
+	const singleTodo = document.createElement('div');
+	singleTodo.className = 'todo';
+
+	const checkBoxAndAddedTodo = document.createElement('div');
+	checkBoxAndAddedTodo.className = 'todo-item';
+	checkBoxAndAddedTodo.innerHTML = `
+    <label class="hamburger">
+        <input type="checkbox"/>
+        <svg viewBox="0 0 32 32" onclick="markAsRead(${index})">
+            <path class="line line-top-bottom"
+                  d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+            <path class="line" d="M7 16 27 16"></path>
+        </svg>
+    </label>
+    <p>${todo.title}</p>
+`;
+
+	singleTodo.appendChild(checkBoxAndAddedTodo);
+
+	const actionButtons = document.createElement('div');
+	actionButtons.className = 'flex';
+	actionButtons.innerHTML = `
+	<button class="button" onclick='showEditModal(${index})'>Edit</button>
+	<button class="delete-button" onclick='deleteTodo(${index})'>Delete</button>
+	`;
+
+	singleTodo.appendChild(actionButtons);
+
+	todosContainer.appendChild(singleTodo);
+}
+
+function markAsRead(index) {
+	const todo = todos[index];
+	todo.completed = !todo.completed;
+	const todoText = document.querySelectorAll('.todo-item p')[index];
+	todoText.classList.toggle('completed');
+}
+
+function showEditModal(index) {
+	const modal = document.createElement('dialog');
+	modal.id = 'edit-todo-modal';
+	modal.innerHTML = `
+		<div class="flex">
+			<h2>Edit Todo</h2>
+			<button class="delete-button">Close</button>
+		</div>
+
+		<form class="edit-todo">
+			<input class="input" type="text" value="${todos[index].title}" />
+			<button class="button">Save</button>
+		</form>
+	`;
+
+	document.body.appendChild(modal);
+	modal.showModal();
+
+	const editTodoForm = document.querySelector('.edit-todo');
+	editTodoForm.addEventListener('submit', (e) => editTodo(e, index));
+
+	const closeButton = document.querySelector('dialog .delete-button');
+	closeButton.addEventListener('click', () => {
+		modal.close();
+		modal.remove();
+	});
+}
+
+function editTodo(e, index) {
+	e.preventDefault();
+	const newTitle = e.target[0].value;
+	todos[index].title = newTitle;
+	fetchAllTodos(todos);
+
+	// Remove the modal from the UI:
+	e.target.parentNode.close();
+	e.target.parentNode.remove();
+}
+
+function deleteTodo(index) {
+	todos.splice(index, 1);
+	fetchAllTodos(todos);
+}
+
+// Add a new item.
+const form = document.querySelector('form');
+form.addEventListener('submit', addNewTodo);
+
+function addNewTodo(e) {
+	e.preventDefault();
+	const inputValue = e.target[0].value;
+	const newTodo = {
+		id: todos.length + 1,
+		title: inputValue,
+		completed: false,
+	};
+
+	todos.push(newTodo);
+	fetchAllTodos(todos);
+	form.reset();
+}
